8dd78a3b3babebc69e502539d0c85caa5090454d
Serhat Kumas <serhatkumas@outlook.com>
2022-10-24 12:55:14 +0300

    Main page is developed

---
+++ src/JsFiles/titleAnimation.js
@@ -0,0 +1,22 @@
+var i = 0;
+var j = 0;
+var txt = 'Serhat Kumas';
+var subTxt= "Software Developer"
+var speed = 65;
+
+function typeWriter() {
+  if(i < txt.length) {
+    document.getElementById("main-logo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  subWriter();
+}
+
+function subWriter() {
+  if(j< subTxt.length) {
+    document.getElementById("sub-logo").innerHTML += subTxt.charAt(j);
+    j++;
+    setTimeout(typeWriter, speed);
+  }
+}
